c358d642fb69966838e803bada0bca2ea79df751
Łukasz Bączkiewicz <147326943+vienox@users.noreply.github.com>
2026-03-16 23:31:22 +0100

    dtos

--- obj/Debug/net10.0/QuizApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f586c75c5520445b605cf211fd61581a223d5783")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2db46474741ab5a3384c6943bb912f78e9adde9")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
