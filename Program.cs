1514c115732a6bc4eb9dbc836f6b9f8f0499e86a
Łukasz Bączkiewicz <147326943+vienox@users.noreply.github.com>
2026-03-24 16:23:15 +0100

    Serve built React assets from ASP.NET

--- Program.cs
+++ Program.cs
@@ -25,7 +25,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
